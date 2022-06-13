e20bbaf850bdf16b1a6d5be9a99fc6b0d54c68a6
Liubomyr Demianchuk <demianchukliubomyr@gmail.com>
2022-06-13 15:57:57 +0200

    implemented services methods getFighters, getFighter, createFighter, updateFighter, deleteFighter and route controllers for work with the Fighter entity

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -1,10 +1,92 @@
-const { Router } = require('express');
-const FighterService = require('../services/fighterService');
-const { responseMiddleware } = require('../middlewares/response.middleware');
-const { createFighterValid, updateFighterValid } = require('../middlewares/fighter.validation.middleware');
+const { Router } = require("express");
+const FighterService = require("../services/fighterService");
+const { responseMiddleware } = require("../middlewares/response.middleware");
+const {
+  createFighterValid,
+  updateFighterValid,
+} = require("../middlewares/fighter.validation.middleware");
+const { FighterRepository } = require("../repositories/fighterRepository");
 
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+
+router.get("/", function (req, res, next) {
+  const result = FighterService.getFighters();
+
+  if (result) {
+    res.send(result);
+  } else {
+    const error = {
+      error: true,
+      message: "Users not found",
+    };
+
+    res.status(404).json(error);
+  }
+});
+
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params;
+  const result = FighterService.getFighter(id);
+
+  if (result) {
+    res.send(result);
+  } else {
+    const error = {
+      error: true,
+      message: "User not found2",
+    };
+
+    res.status(404).json(error);
+  }
+});
+
+router.post("/", function (req, res, next) {
+  const result = FighterService.createFighter(req.body);
+
+  if (result) {
+    res.send(result);
+  } else {
+    const error = {
+      error: true,
+      message: "User not create",
+    };
+
+    res.status(404).json(error);
+  }
+});
+
+router.put("/:id", function (req, res, next) {
+  const { id } = req.params;
+  const result = FighterService.updateFighter(id, req.body);
+
+  if (result) {
+    res.send(result);
+  } else {
+    const error = {
+      error: true,
+      message: "User not create",
+    };
+
+    res.status(404).json(error);
+  }
+});
+
+router.delete("/:id", function (req, res, next) {
+  const { id } = req.params;
+  const result = FighterService.deleteFighter(id);
+
+  if (result) {
+    res.send(result);
+  } else {
+    const error = {
+      error: true,
+      message: "User not create",
+    };
+
+    res.status(404).json(error);
+  }
+});
 
 module.exports = router;
--- services/fighterService.js
+++ services/fighterService.js
@@ -9,6 +9,36 @@ class FighterService {
     }
     return req;
   }
+
+  getFighters() {
+    const fighters = FighterRepository.getAll();
+    return this.checkDbRespounse(fighters);
+  }
+
+  getFighter(id) {
+    const fighter = this.search({ id });
+    return this.checkDbRespounse(fighter);
+  }
+
+  createFighter(data) {
+    const fighter = FighterRepository.create(data);
+    return this.checkDbRespounse(fighter);
+  }
+
+  updateFighter(id, dataToUpdate) {
+    const fighter = FighterRepository.update(id, dataToUpdate);
+    return this.checkDbRespounse(fighter);
+  }
+
+  deleteFighter(id) {
+    const fighter = FighterRepository.delete(id);
+    return this.checkDbRespounse(fighter);
+  }
+
+  search(search) {
+    const item = FighterRepository.getOne(search);
+    return this.checkDbRespounse(item);
+  }
 }
 
 module.exports = new FighterService();
