44dd54f974bc0bed3246d23065ffc12c4d857ebb
Liubomyr Demianchuk <demianchukliubomyr@gmail.com>
2022-06-13 11:41:38 +0200

    implemented services methods getUsers, getUser, createUser and route controllers for work with the User entity

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,56 @@
-const { Router } = require('express');
-const UserService = require('../services/userService');
-const { createUserValid, updateUserValid } = require('../middlewares/user.validation.middleware');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+const { Router } = require("express");
+const UserService = require("../services/userService");
+const { createUserValid, updateUserValid } = require("../middlewares/user.validation.middleware");
+const { responseMiddleware } = require("../middlewares/response.middleware");
 
 const router = Router();
 
 // TODO: Implement route controllers for user
+
+router.get("/", function (req, res, next) {
+  const result = UserService.getUsers();
+
+  if (result) {
+    res.send(result);
+  } else {
+    const error = {
+      error: true,
+      message: "Users not found",
+    };
+
+    res.status(404).json(error);
+  }
+});
+
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params;
+  const result = UserService.getUser(id);
+
+  if (result) {
+    res.send(result);
+  } else {
+    const error = {
+      error: true,
+      message: "User not found2",
+    };
+
+    res.status(404).json(error);
+  }
+});
+
+router.post("/", function (req, res, next) {
+  const result = UserService.createUser(req.body);
+
+  if (result) {
+    res.send(result);
+  } else {
+    const error = {
+      error: true,
+      message: "User not create",
+    };
+
+    res.status(404).json(error);
+  }
+});
 
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -1,16 +1,37 @@
-const { UserRepository } = require('../repositories/userRepository');
+const { UserRepository } = require("../repositories/userRepository");
 
 class UserService {
+  // TODO: Implement methods to work with user
 
-    // TODO: Implement methods to work with user
-
-    search(search) {
-        const item = UserRepository.getOne(search);
-        if(!item) {
-            return null;
-        }
-        return item;
+  checkDbRespounse(req) {
+    if (!req) {
+      return null;
     }
+    return req;
+  }
+
+  getUsers() {
+    const users = UserRepository.getAll();
+    return this.checkDbRespounse(users);
+  }
+
+  getUser(id) {
+    const user = this.search({ id });
+    return this.checkDbRespounse(user);
+  }
+
+  createUser(data) {
+    const user = UserRepository.create(data);
+    return this.checkDbRespounse(user);
+  }
+
+  search(search) {
+    const item = UserRepository.getOne(search);
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
 }
 
 module.exports = new UserService();
